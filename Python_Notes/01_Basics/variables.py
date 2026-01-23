8ee3e894870f65b51e41f3ac937fefaeada8d7d0
Divyajot Kaur <d.jotkaur23@gmail.com>
2026-01-23 12:06:22 +0530

    Update variables.py

--- Python_Notes/01_Basics/variables.py
+++ Python_Notes/01_Basics/variables.py
@@ -1,1 +1,33 @@
+# Variable is a container that stores a value, which can be later used in the code.
+# Basically it is a name given to a location in computer's memory that holds data.
+# Variable improve code readability and make code easier to manage.
 
+# Example 
+disney_char1 = "Mickey Mouse"
+disney_char2 = "Woody"
+print(disney_char1)
+print(disney_char2)
+
+# We can assign multiple variable names in single line
+princess1, princess2, princess3 = "Rapunzel", "Mulan", "Jasmine"
+print(princess1)
+print(princess2)
+print(princess3)
+
+# Checking the type of variable used
+# Imagine a student and based on his marks the result he obtained
+name = "Josh"
+age = 23
+percentage_obtained = 86.9
+result = True
+
+# Printing the type of variable
+print(type(name))                    # String
+print(type(age))                     # Integer
+print(type(percentage_obtained))     # Float
+print(type(result))                  # Boolean
+
+# Complex Numbers
+num1 = 1+3j
+print(num1)
+print(type(num1))
