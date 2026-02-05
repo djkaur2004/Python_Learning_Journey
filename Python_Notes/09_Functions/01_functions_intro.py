d4fe7e1886ce70759561d7b00525d607babf1681
Divyajot Kaur <d.jotkaur23@gmail.com>
2026-02-05 17:40:02 +0530

    Update 01_functions_intro.py

--- Python_Notes/09_Functions/01_functions_intro.py
+++ Python_Notes/09_Functions/01_functions_intro.py
@@ -1,1 +1,23 @@
+# What are Function in Python
+'''A function is a reusable block of code that performs a specific task.
+Instead of writing the same code again and again, you put it inside a function and call it whenever you need it.'''
 
+# Why do we use functions?
+'''Functions help to:
+✅ Avoid repetition
+✅ Make code cleaner and readable
+✅ Break a big problem into smaller pieces
+✅ Debug and maintain code easily'''
+
+# Syntax
+'''
+def function_name(parameters):
+    # code block
+    return result
+    
+where,
+def → keyword used to define a function
+function_name → name of the function
+parameters → inputs (optional)
+return → sends the output back (optional)
+'''
