441de641c37a87a00d6a817548bd8638fd0e5958
Divyajot Kaur <d.jotkaur23@gmail.com>
2026-02-11 17:16:21 +0530

    Update 01_enumerate_func_intro.py

--- Python_Notes/11_Enumerate_Functions/01_enumerate_func_intro.py
+++ Python_Notes/11_Enumerate_Functions/01_enumerate_func_intro.py
@@ -1,1 +1,18 @@
+# Enumerate Functions
+''' The enumerate function is a built-in function in python that allows you to loop over a sequence(such as list, tuple, or string)
+and get the index and value at the same time.'''
+
+# Why do we need enumerate()?
+# Code without using enumerate()
+fruits = ["apple", "banana", "cherry"]
+
+for i in range(len(fruits)):
+    print(i, fruits[i])
+
+# Using enumerate()
+fruits = ["apple", "banana", "cherry"]
+
+for index, value in enumerate(fruits):
+    print(index, value)
+# here the code is more readable and cleaner
 
