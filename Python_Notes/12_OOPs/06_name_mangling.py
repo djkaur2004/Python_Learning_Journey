be2dd31df3494ab73242a968979ceed67fe794ec
Divyajot Kaur <d.jotkaur23@gmail.com>
2026-02-20 15:42:29 +0530

    Update 06_name_mangling.py

--- Python_Notes/12_OOPs/06_name_mangling.py
+++ Python_Notes/12_OOPs/06_name_mangling.py
@@ -1,1 +1,25 @@
+# Name Mangling in Python
+'''Name mangling is the process where Python automatically changes the name of a private variable (with double underscores __) 
+to avoid accidental access or modification outside the class.
 
+Python does this to make private variables harder to access, not completely impossible.'''
+
+# Example
+class BankAccount:
+    def __init__(self):
+        self.__balance = 1000   # private variable
+
+account = BankAccount()
+
+# Direct access ❌
+# print(account.__balance)  → AttributeError
+
+# Using name mangling ✅
+print(account._BankAccount__balance)   # Output: 1000
+
+# Why Python Uses Name Mangling
+'''
+✔ Prevents accidental override in subclasses
+✔ Protects sensitive data
+✔ Avoids name conflicts
+'''
