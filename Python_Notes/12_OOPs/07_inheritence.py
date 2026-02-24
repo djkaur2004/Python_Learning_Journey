fbbbcabd7d10b2e71ab44acb6e0daf393068f4d2
Divyajot Kaur <d.jotkaur23@gmail.com>
2026-02-24 11:36:22 +0530

    Update 07_inheritence.py

--- Python_Notes/12_OOPs/07_inheritence.py
+++ Python_Notes/12_OOPs/07_inheritence.py
@@ -1,1 +1,22 @@
+# What is Inheritence
+'''Inheritance is an OOP concept where a child class (subclass) can acquire the properties and methods of a parent class (superclass).'''
 
+# Example
+class Animal:    # Parent Class/ Base Class
+    def speak(self):
+        print("Animal makes a sound")
+
+class Dog(Animal):   # Dog inherits from Animal    # Child Class/ Derived Class
+    def bark(self):
+        print("Dog barks")
+
+d = Dog()
+d.speak()   # inherited method
+d.bark()
+
+# Output
+'''
+Animal makes a sound
+Dog barks'''
+
+# Types of Inheritence
