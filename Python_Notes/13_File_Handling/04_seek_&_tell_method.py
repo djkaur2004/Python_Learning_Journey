e54318892f486805f4f7f2d8fab472db343a53aa
Divyajot Kaur <d.jotkaur23@gmail.com>
2026-03-19 13:35:09 +0530

    Update 04_seek_&_tell_method.py

--- Python_Notes/13_File_Handling/04_seek_&_tell_method.py
+++ Python_Notes/13_File_Handling/04_seek_&_tell_method.py
@@ -1,1 +1,33 @@
+# Seek and Tell Methods
+
+# 1. seek() Method
+'''It moves the file pointer to a specific position.'''
+
+# Example
+file = open("example.txt", "r")
+
+file.seek(0)   # move to beginning
+print(file.read())
+
+file.close()
+
+# Another example with position
+file = open("example.txt", "r")
+
+file.seek(6)
+print(file.read())
+
+file.close()
+
+# 2. tell() Method
+'''It returns the current position of the file pointer.'''
+
+# Example
+file = open("example.txt", "r")
+
+print(file.tell())   # position before reading
+file.read(5)
+print(file.tell())   # position after reading
+
+file.close()
 
