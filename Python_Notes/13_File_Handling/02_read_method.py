e4b533e2595f4a14e8b7f441cc14fb464f2027b9
Divyajot Kaur <d.jotkaur23@gmail.com>
2026-03-19 13:29:35 +0530

    Update 02_read_method.py

--- Python_Notes/13_File_Handling/02_read_method.py
+++ Python_Notes/13_File_Handling/02_read_method.py
@@ -1,1 +1,22 @@
+# Read Method
 
+# 1. read -> Reads the entire file
+file = open("example.txt", "r")
+print(file.read())
+file.close()
+
+#  2. readline -> Reads one line at a time
+file = open("example.txt", "r")
+
+print(file.readline())   # first line
+print(file.readline())   # second line
+
+file.close()
+
+# 3. readlines -> Reads all lines and return a list
+file = open("example.txt", "r")
+
+lines = file.readlines()
+print(lines)
+
+file.close()
