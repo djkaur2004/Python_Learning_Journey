1ebb819ea1087b2f45e2c1ce5f87ce827c87bbe3
Divyajot Kaur <d.jotkaur23@gmail.com>
2026-03-19 13:37:45 +0530

    Update 05_with_statement.py

--- Python_Notes/13_File_Handling/05_with_statement.py
+++ Python_Notes/13_File_Handling/05_with_statement.py
@@ -1,1 +1,6 @@
+# with statement
+'''with statement automatically closes the file i.e. it is not necessary to do it manually using close().'''
 
+with open("example.txt", "r") as file:
+    content = file.read()
+    print(content)
