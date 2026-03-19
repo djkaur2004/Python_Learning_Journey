8fe3e23302a4a3afb6ec7ec32ff8e1e57522fb9f
Divyajot Kaur <d.jotkaur23@gmail.com>
2026-03-19 13:31:16 +0530

    Update 03_write_method.py

--- Python_Notes/13_File_Handling/03_write_method.py
+++ Python_Notes/13_File_Handling/03_write_method.py
@@ -1,1 +1,17 @@
+# Write Method
 
+# 1. write -> Writes data to a file
+file = open("example.txt", "w")
+
+file.write("Hello\n")
+file.write("Python")
+
+file.close()
+
+# 2. writelines -> Write multiple lines (list)
+file = open("example.txt", "w")
+
+lines = ["Line 1\n", "Line 2\n", "Line 3\n"]
+file.writelines(lines)
+
+file.close()
